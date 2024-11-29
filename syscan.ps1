b73989448639c5f93a7526a2c74b99338cfc0db6
crimsonKss <rapha@raphap3.net>
2024-11-29 14:07:48 -0300

    Updated script for elevation checking

--- syscan.ps1
+++ syscan.ps1
@@ -1,5 +1,22 @@
-Start-Process PowerShell -ArgumentList "Write-Host '(1/4) Chkdsk' -ForegroundColor Gray -BackgroundColor DarkGreen; chkdsk /scan;
-Write-Host '`n(2/4) SFC - 1st run' -ForegroundColor Gray -BackgroundColor DarkGreen; sfc /scannow;
-Write-Host '`n(3/4) DISM' -ForegroundColor Gray -BackgroundColor DarkGreen; DISM /Online /Cleanup-Image /RestoreHealth;
-Write-Host '`n(4/4) SFC - 2nd run' -ForegroundColor DarkGreen; sfc /scannow;
-Read-Host '`n`nPress Enter to Continue'" -Verb runas
+$isAdmin = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (-not $isAdmin) {
+    try {
+        $command = $MyInvocation.MyCommand.Definition
+        Start-Process powershell -Verb RunAs -Wait -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command $command"
+        exit
+    }
+    catch {
+        Clear-Host;
+        Write-Host "---ERROR!!---" -ForegroundColor White -BackgroundColor Red;
+        Write-Host "You need admin rights to run this script.`n"
+        exit 1
+    }
+}
+
+Clear-Host;
+Write-Host "(1/4) Chkdsk" -ForegroundColor Gray -BackgroundColor DarkGreen; chkdsk /scan;
+Write-Host "`n(2/4) SFC - 1st run" -ForegroundColor Gray -BackgroundColor DarkGreen; sfc /scannow;
+Write-Host "`n(3/4) DISM" -ForegroundColor Gray -BackgroundColor DarkGreen; DISM /Online /Cleanup-Image /RestoreHealth;
+Write-Host "`n(4/4) SFC - 2nd run" -ForegroundColor DarkGreen; sfc /scannow;
+Read-Host "`n`nPress Enter to Continue";
